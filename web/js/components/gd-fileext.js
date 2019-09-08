0a7efdc55af5bf004983976bdd40fdf7f5ad747f
Douglas Fils <drfils@gmail.com>
2019-09-07 21:27:12 -0500

    updates to the web components

--- web/js/components/gd-fileext.js
+++ web/js/components/gd-fileext.js
@@ -35,14 +35,16 @@
                   `;
             } else {
                 var ext = obj.name.split('.').pop();
+                var dist = obj.distribution.contentUrl;
+
 
                 this.shadowRoot.innerHTML = `
                     <style>
                           p {
                           }
                     </style>
-                    <div>
-                       <a target="_blank" href="http://geodex.org">
+                    <div style="height:75px">
+                       <a target="_blank" href="${dist}">
 
 <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 43 46">
 <defs>
--- web/js/components/gio-tangram.js
+++ web/js/components/gio-tangram.js
@@ -30,16 +30,26 @@ import {
             console.log(shape);
             var url = new URL(window.location.href);
             var urlparam = url.searchParams.get("url");
-            var geturl = "https://tangram.gleaner.io/ucheck?url=" +  urlparam + "&format=human&shape=" + shape;
 
-            // GET test call...
-            tj_providers(geturl).then((providers) => {
-                // do something with the providers
+            if (urlparam) {
+                var geturl = "https://tangram.gleaner.io/ucheck?url=" +  urlparam + "&format=human&shape=" + shape;
+
+                // GET test call...
+                tj_providers(geturl).then((providers) => {
+                    // do something with the providers
+                    this.attachShadow({ mode: 'open' });
+                    // var h =  `<div>${itemTemplates}</div>`;
+                    this.shadowRoot.innerHTML = `<div style="width=100%;overflow:auto;"><pre>${providers}</pre></div>` ;
+                    // this.shadowRoot.appendChild(this.cloneNode(h));
+                });
+            }
+            else {
                 this.attachShadow({ mode: 'open' });
                 // var h =  `<div>${itemTemplates}</div>`;
-                this.shadowRoot.innerHTML = `<div style="width=100%;overflow:auto;"><pre>${providers}</pre></div>` ;
-                // this.shadowRoot.appendChild(this.cloneNode(h));
-            });
+                this.shadowRoot.innerHTML = `<div style="width=100%;overflow:auto;"><pre>No URL provided to check</pre></div>` ;
+            }
+
+          
         }
     }
     window.customElements.define('gleaner-tangram', Tangram);
